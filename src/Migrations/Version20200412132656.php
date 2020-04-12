a208b30a4c8a6ceebd7ccd3599d7ef8141ce5b23
root <root@vps723401.ovh.net>
2020-04-12 20:36:19 +0200

    add Doctrine migration dependency

---
+++ src/Migrations/Version20200412132656.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200412132656 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+     
+
+    }
+}
